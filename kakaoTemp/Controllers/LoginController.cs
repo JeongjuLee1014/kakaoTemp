bb2814518bc8269a1f094a613f8a34b9d5992db5
jeongju <ryan20115@naver.com>
2025-01-03 17:05:32 +0900

    로그인 구현 준비

--- kakaoTemp/Controllers/LoginController.cs
+++ kakaoTemp/Controllers/LoginController.cs
@@ -7,11 +7,11 @@ namespace kakaoTemp.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("/kakao")]
         public IActionResult RedirectToKakaoLogin()
         {
             const string client_id = "49209eb683ce3a79ad35d14c2dc39b60";
-            const string redirect_uri = "https://localhost:7032/oauth";
+            const string redirect_uri = "https://localhost:7032/oauth/kakao";
             const string response_type = "code";
 
             const string redirectUrl = "https://kauth.kakao.com/oauth/authorize" +
--- kakaoTemp/Controllers/OAuthController.cs
+++ kakaoTemp/Controllers/OAuthController.cs
@@ -11,19 +11,53 @@ namespace kakaoTemp.Controllers
     {
         private readonly OAuthService _oauthService;
 
+        // 로그인 플랫폼에 대한 상수 정의
+        public enum Platform
+        {
+            Kakao = 1,
+            Google,
+            Naver
+        }
+
         public OAuthController(OAuthService oauthService)
         {
             _oauthService = oauthService;
         }
 
-        [HttpGet]
+        [HttpGet("/kakao")]
         public async Task<IActionResult> HandleOAuthRedirect([FromQuery] string code)
         {
             string access_token = await _oauthService.RequestAccessToken(code);
             long user_id = await _oauthService.RequestUserId(access_token);
-            // 회원 가입 또는 로그인 로직
+            user_id = long.Parse(Platform.Kakao.ToString() + user_id.ToString());
+
+            if (isJoined(user_id))
+            {
+                Login();
+            }
+            else
+            {
+                Join();
+            }
+
             var loginCompleteResponse = await System.IO.File.ReadAllTextAsync("./loginCompletePage.html");
             return Content(loginCompleteResponse, "text/html");
+        }
+
+        public bool isJoined(long user_id)
+        {
+            Console.WriteLine(user_id);
+            return true;
+        }
+
+        public void Login()
+        {
+            Console.WriteLine("로그인");
+        }
+
+        public void Join()
+        {
+            Console.WriteLine("회원 가입");
         }
     }
 }
